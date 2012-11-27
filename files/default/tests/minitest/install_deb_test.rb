7d440768f9429de2c0aefa4f031c07fc304b9436
Jorge Bianquetti <jbianquetti@gmail.com>
2012-11-27 12:10:42 +0100

    Fix typo error

--- files/default/tests/minitest/install_deb_test.rb
+++ files/default/tests/minitest/install_deb_test.rb
@@ -1,14 +1,14 @@
 require 'minitest/spec' 
 require File.expand_path('../support/helpers', __FILE__)
 
-describe_recipe 'arangodb::installdeb' do
+describe_recipe 'arangodb::install_deb' do
   include Helpers::Arangodb
    describe "resource apt_repository" do 
     it "creates sources archive" do
       file("/etc/apt/sources.list.d/arangodb-source.list").must_include 'www.arangodb.org/repositories'
     end
   end
-   describe "resource package" fo 
+   describe "resource package" do 
     it "package arangodb must be installed" do
       package("arangodb").must_be_installed 
     end
