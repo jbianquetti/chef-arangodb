7e30a6fbe31699c1f2bc2316d983eb3b14090dff
Jorge Bianquetti <jbianquetti@gmail.com>
2012-11-27 12:08:12 +0100

    Test if package is really installed

--- files/default/tests/minitest/install_deb_test.rb
+++ files/default/tests/minitest/install_deb_test.rb
@@ -3,11 +3,16 @@ require File.expand_path('../support/helpers', __FILE__)
 
 describe_recipe 'arangodb::installdeb' do
   include Helpers::Arangodb
-   describe "/etc/apt/sources.list.d/arangodb-source.list" do 
+   describe "resource apt_repository" do 
     it "creates sources archive" do
       file("/etc/apt/sources.list.d/arangodb-source.list").must_include 'www.arangodb.org/repositories'
     end
   end
+   describe "resource package" fo 
+    it "package arangodb must be installed" do
+      package("arangodb").must_be_installed 
+    end
+   end 
 
 
 end  
